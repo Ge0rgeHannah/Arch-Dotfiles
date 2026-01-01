7391c52f059716e5d032bfdbd2bd112a54f67b90
George Hannah <80538951+Ge0rgeHannah@users.noreply.github.com>
2026-01-01 17:25:35 +0000

    fix: rewrote random wallpaper selector

--- hypr/random_wallpaper.sh
+++ hypr/random_wallpaper.sh
@@ -1,3 +1,15 @@
 #!/bin/zsh
 
-waypaper --random
+MONITOR="DP-1"
+WALLPAPER_DIR="/home/george/.config/hypr/Wallpapers"
+
+
+files=("$WALLPAPER_DIR"/*(.N) "$WALLPAPER_DIR"/*(@N))
+
+if (( ${#files} == 0 )); then
+	printf "No files found in" "$WALLPAPER_DIR" >&2
+	exit 1
+fi
+random_file="${files[RANDOM % ${#files[@]} + 1]}"
+
+hyprctl hyprpaper wallpaper "${MONITOR}, ${random_file}"
